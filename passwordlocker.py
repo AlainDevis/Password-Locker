6f9c7a9a7c341c28c66dd280263a6273c7470752
AlainDevis <devisalain04@gmail.com>
2020-11-23 09:43:31 +0200

    defining the user class

--- passwordlocker.py
+++ passwordlocker.py
@@ -14,7 +14,8 @@ class User:
         print("Enter password")
         self.p = input()
         if (self.u == self.username and self.p == self.password):
-            print("correct credentials")
+            self.usercredentials = Credentials()
+            self.usercredentials.store()
         else:
             print("invalid username or password")
 
@@ -22,21 +23,28 @@ class Credentials:
     """
     """
     user_list = []
-    def __init__(self,account_name,username,password):
-        """
-        """
-        self.account_name = account_name
-        self.username = username
-        self.password = password
+    # def __init__(self,account_name,username,password):
+    #     """
+    #     """
+    #     self.account_name = account_name
+    #     self.username = username
+    #     self.password = password
     
     def store(self):
+        self.acc_credential = []
         print("Enter the account name like twitter")
         acc =input()
+        self.acc_credential.append(acc)
         print("Enter the username of your account")
         acc_username =input()
+        self.acc_credential.append(acc_username)
         print("Enter the password of your account")
         acc_password =input()
+        self.acc_credential.append(acc_password)
+        self.user_list.append(self.acc_credential)
+        print(self.user_list)
 
+    def 
 
 
 userinfo = User("alain","devis123")
